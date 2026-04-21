a816ec389b2d7b6615d9fdf6a3d3d1d77a760d68
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2026-04-21 16:03:54 +0700

    fix: use updated react compiler preset config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import babel from "vite-plugin-babel";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
@@ -14,11 +14,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    babel({
-      babelConfig: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
 });
 
