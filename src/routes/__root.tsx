f6be7c14d37fcdff6743ec7bc45d1a13c5add591
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2026-04-21 15:39:16 +0700

    chore: initial tanstack start project commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,53 @@
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import type { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+import { HeadContent, Scripts, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+
+import appCss from "../styles.css?url";
+
+interface RouterContext {
+  queryClient: QueryClient;
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  head: () => ({
+    meta: [
+      { title: "SIX Planner" },
+      { charSet: "utf-8" },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+    ],
+    links: [{ rel: "stylesheet", href: appCss }],
+  }),
+  shellComponent: RootDocument,
+});
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <TanStackDevtools
+          config={{ position: "bottom-right", hideUntilHover: true }}
+          plugins={[
+            {
+              name: "Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            {
+              name: "Query",
+              render: <ReactQueryDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    babel({
+      babelConfig: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
+});
+
+export default config;
